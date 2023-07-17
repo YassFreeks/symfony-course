ca4d282f93451573df3d471238196b96b3de4772
YassFreeks <y.moutaouakil@hotmail.com>
2023-07-17 12:33:32 +0200

    views i symfony

--- movies/src/Controller/MoviesController.php
+++ movies/src/Controller/MoviesController.php
@@ -9,12 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies/{name}', name: 'movies',defaults: ['name' => null], methods:['GET', 'HEAD'])]
-    public function index($name): Response
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => $name,
-            'path' => 'src/Controller/MoviesController.php',
+        return $this->render('index.html.twig', [
+            'title' => 'Avengers: Endgame'
         ]);
     }
 }
