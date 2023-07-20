76fd41fe57f612d58e3ba2319dd27234f369b13f
YassFreeks <y.moutaouakil@hotmail.com>
2023-07-20 11:53:16 +0200

    How to use images

--- movies/src/Controller/MoviesController.php
+++ movies/src/Controller/MoviesController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+
+    #[Route('/movies', name: 'movies')]
     public function index(): Response
     {
-        return $this->render('movies/index.html.twig', [
-            'controller_name' => 'MoviesController',
-        ]);
+
+        return $this->render('index.html.twig');
     }
 }
